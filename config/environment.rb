3eba5e4322dbac3e9f523929be010a34b8b9ad40
Jasper St. Pierre <jasperstpierre@hotmail.com>
2022-11-11 00:22:04 -0500

    change database to postgres, set up env variables

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative "application"
 
+env_file = File.join(Rails.root, "config", "env.rb")
+load(env_file) if File.exist?(env_file)
+
 # Initialize the Rails application.
 Rails.application.initialize!
