cf5b91efed04fecad9512e04883bfb37ede87082
Jasper St. Pierre <jasperstpierre@hotmail.com>
2022-11-12 19:19:11 -0500

    set up summoners controller, create api service

--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -1,2 +1,15 @@
 class Summoner < ApplicationRecord
+  enum region: {
+    na1: 0,
+    euw1: 1,
+    eun1: 2,
+    kr: 3,
+    jp1: 4,
+    oc1: 5,
+    la1: 6,
+    la2: 7,
+    tr1: 8,
+    br1: 9,
+    ru: 10
+  }
 end
