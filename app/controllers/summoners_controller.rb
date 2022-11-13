48a579a34c0ec574481284ffbe3e3638b777b645
Jasper St. Pierre <jasperstpierre@hotmail.com>
2022-11-13 13:42:31 -0500

    feat: error handling

--- app/controllers/summoners_controller.rb
+++ app/controllers/summoners_controller.rb
@@ -2,12 +2,18 @@ class SummonersController < ApplicationController
   before_action :set_summoner
 
   def show
-    @summoner = Summoner.first
+    @text = @summoner.to_s if @summoner.present?
   end
 
   private
 
   def set_summoner
-    # TODO
+    begin
+      @summoner = Riot::ApiService.get_summoner_by_name(region: params[:region], name: params[:name])
+    rescue Riot::ApiService::NotFoundError
+      @text = "summoner not found"
+    rescue => error
+      @text = error.message
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :summoners, param: :name, only: [:show]
+  get "/summoners/:region/:name", to: "summoners#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
