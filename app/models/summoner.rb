62781b19d8d96e84f6d2291144a2b18156d6732f
Jasper St. Pierre <jasperstpierre@hotmail.com>
2022-11-18 01:19:19 -0500

    feat: support region switching, case insensitive search

--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -2,6 +2,8 @@ class Summoner < ApplicationRecord
   validates :puuid, uniqueness: true
   validates :name, uniqueness: { scope: :region, message: "Summoner already exists for this region." }
 
+  scope :by_name, ->(name) { where("lower(name) = ?", name.strip.downcase) }
+
   enum region: {
     na1: 0,
     euw1: 1,
--- app/services/summoner_service.rb
+++ app/services/summoner_service.rb
@@ -1,8 +1,13 @@
 class SummonerService
   def self.get_summoner_by_name(region:, name:)
     begin
-      Summoner.find_or_create_by(region: region, name: name) do |summoner|
+      name = name.gsub(/\+/, " ")
+      summoner = Summoner.create_with(name: name).by_name(name).find_or_initialize_by(region: region)
+
+      if summoner.new_record?
         update_summoner(summoner: summoner)
+      else
+        summoner
       end
     rescue Riot::ApiService::NotFoundError
       nil
@@ -12,11 +17,21 @@ class SummonerService
   def self.update_summoner(summoner:)
     summoner_object = Riot::ApiService.get_summoner_by_name(region: summoner.region, name: summoner.name)
 
-    summoner.update(
+    summoner_params = {
+      name: summoner_object["name"],
       encrypted_id: summoner_object["id"],
       puuid: summoner_object["puuid"],
       profile_icon_id: summoner_object["profileIconId"],
       level: summoner_object["summonerLevel"]
-    )
+    }
+
+    if summoner.update(summoner_params)
+      summoner
+    else
+      existing_summoner = Summoner.find_by(puuid: summoner.puuid)
+      summoner_params[:region] = summoner.region
+      existing_summoner.update(summoner_params)
+      existing_summoner
+    end
   end
 end
