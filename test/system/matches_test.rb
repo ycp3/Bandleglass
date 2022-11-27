ef34611d9c79eb1a87147785ddd5dec8f9e58770
ycp3 <jasperstpierre@hotmail.com>
2022-11-26 22:04:04 -0500

    scaffold matches

---
+++ test/system/matches_test.rb
@@ -0,0 +1,39 @@
+require "application_system_test_case"
+
+class MatchesTest < ApplicationSystemTestCase
+  setup do
+    @match = matches(:one)
+  end
+
+  test "visiting the index" do
+    visit matches_url
+    assert_selector "h1", text: "Matches"
+  end
+
+  test "should create match" do
+    visit matches_url
+    click_on "New match"
+
+    click_on "Create Match"
+
+    assert_text "Match was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Match" do
+    visit match_url(@match)
+    click_on "Edit this match", match: :first
+
+    click_on "Update Match"
+
+    assert_text "Match was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Match" do
+    visit match_url(@match)
+    click_on "Destroy this match", match: :first
+
+    assert_text "Match was successfully destroyed"
+  end
+end
