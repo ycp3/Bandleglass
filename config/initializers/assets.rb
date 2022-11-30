be48c60d3d37b6df9915b5492a04874cc7f9639f
Jasper St. Pierre <jasperstpierre@hotmail.com>
2022-11-30 00:53:30 -0500

    download tarball on startup

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,10 +3,18 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 
+FileUtils.mkdir_p(Rails.root.join("vendor", "assets", "images"))
+FileUtils.mkdir_p(Rails.root.join("vendor", "assets", "data"))
+FileUtils.mkdir_p(Rails.root.join("vendor", "assets", "raw"))
+
 # Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "images")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.after_initialize do
+  Riot::DDragonService.update!
+end
