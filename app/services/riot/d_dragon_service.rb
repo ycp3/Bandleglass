5d8f8540c9ae58f2c7727146d3c955a431035e9f
Jasper St. Pierre <jasperstpierre@hotmail.com>
2022-12-01 21:54:41 -0500

    feat: download raw

--- app/services/riot/d_dragon_service.rb
+++ app/services/riot/d_dragon_service.rb
@@ -23,19 +23,23 @@ module Riot
     end
 
     def self.download_raw
-      FileUtils.rm_r Dir.glob("#{dir_raw}/*")
+      FileUtils.rm_r Dir.glob(dir_raw.join("*"))
 
       version = get_latest_version
       uri = URI(base_url + "cdn/dragontail-#{version}.tgz")
 
       Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
         File.open(dir_raw + version, "wb") do |file|
+          total_downloaded = 0
+          puts "Downloading: #{uri.to_s}"
+          print "Downloaded 0MB"
           http.request_get(uri.path) do |response|
             response.read_body do |chunk|
-              puts "Downloaded #{chunk.length} bytes"
+              print "\rDownloaded #{((total_downloaded += chunk.length) / 1048576.0).round(2)}MB"
               file.write chunk
             end
           end
+          puts "\nDownload complete!"
         end
       end
     end
