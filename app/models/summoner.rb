c60f6a6e98662f9d8b3cf17c15401412ad566eb6
Jasper St. Pierre <jasperstpierre@hotmail.com>
2022-12-18 01:24:01 -0500

    refactor: move region to concern

--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -1,20 +1,8 @@
 class Summoner < ApplicationRecord
+  include Regionable
+
   validates :puuid, uniqueness: true
   validates :name, uniqueness: { scope: :region, message: "Summoner already exists for this region." }
 
   scope :by_name, ->(name) { where("lower(name) = ?", name.strip.downcase) }
-
-  enum region: {
-    na1: 0,
-    euw1: 1,
-    eun1: 2,
-    kr: 3,
-    jp1: 4,
-    oc1: 5,
-    la1: 6,
-    la2: 7,
-    tr1: 8,
-    br1: 9,
-    ru: 10
-  }
 end
