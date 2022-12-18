bc301fd4d55193d84a22551907ac4c801f577dc9
Jasper St. Pierre <jasperstpierre@hotmail.com>
2022-12-18 18:09:02 -0500

    helper methods for champion spells, image paths

--- app/models/champion.rb
+++ app/models/champion.rb
@@ -1,3 +1,33 @@
+# frozen_string_literal: true
+
 class Champion < ApplicationRecord
-  has_many :spells
+  has_many :spells, -> { order(:spell_type) }
+
+  def name_and_title
+    "#{name}, #{title}"
+  end
+
+  def q
+    spells.first
+  end
+
+  def w
+    spells.second
+  end
+
+  def e
+    spells.third
+  end
+
+  def r
+    spells.fourth
+  end
+
+  def passive
+    spells.fifth
+  end
+
+  def image_path
+    "champion_icons/#{internal_name}.png"
+  end
 end
