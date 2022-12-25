16a2a38f3936872fa11da18766268ecd56f844f5
Jasper St. Pierre <jasperstpierre@hotmail.com>
2022-12-25 05:39:03 +0000

    feat: dockerize

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the Rails application.
 require_relative "application"
 
-env_file = File.join(Rails.root, "config", "env.rb")
-load(env_file) if File.exist?(env_file)
-
 # Initialize the Rails application.
 Rails.application.initialize!
