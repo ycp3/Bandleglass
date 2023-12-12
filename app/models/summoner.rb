aa50ec6e1d4e2a42ee01db488b220b47baaf6a28
Jasper St. Pierre <jasperstpierre@hotmail.com>
2023-12-12 04:39:28 +0000

    fix: remove name uniqueness validation with change to riot tags

--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -4,7 +4,6 @@ class Summoner < ApplicationRecord
   include Regionable
 
   validates :puuid, uniqueness: true
-  validates :name, uniqueness: { scope: :region, message: "Summoner already exists for this region." }
 
   scope :by_name, ->(name) { where("lower(name) = ?", name.strip.downcase) }
 
